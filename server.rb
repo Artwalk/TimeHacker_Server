69fa539ac3ed8617fd1757164255b501daaa6626
Artwalk <frank.holy.art@gmail.com>
2014-08-25 21:31:28 +0800

    save data to 'post.txt'

--- server.rb
+++ server.rb
@@ -1,5 +1,26 @@
 require 'sinatra'
 
 get '/' do 
-	"Hello, world!"
+	"<p>This is for TimeHacker Server!<p>%s" % readData
+end
+
+post '/feedback' do
+	data = params[:data]
+	saveData(data)
+end
+
+$fName = "post.txt"
+
+def saveData(data)
+	# coding: utf-8 
+	f = File.open($fName, "a")
+	f.puts data
+	f.close
+end
+
+def readData
+	File.open($fName, "r") { |f|
+		s = f.read
+	}
+	
 end
