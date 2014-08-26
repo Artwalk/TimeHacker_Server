65a32f6a840b1db71e37d4238444902e15b92df3
Artwalk <frank.holy.art@gmail.com>
2014-08-26 15:25:31 +0800

    string to formatted JSON data

--- server.rb
+++ server.rb
@@ -1,26 +1,32 @@
 require 'sinatra'
+require 'json'
+require 'fileutils'
+#require 'time'
 
 get '/' do 
-	"<p>This is for TimeHacker Server!<p>%s" % readData
+	content_type :json
+  dict = readData
+  return dict
 end
 
 post '/feedback' do
 	data = params[:data]
-	saveData(data)
+	saveData("{\"%s\" : %s}," % [Time.now, data])
 end
 
 $fName = "post.txt"
 
+
+# r w file
 def saveData(data)
-	# coding: utf-8 
+	# coding: utf-8
 	f = File.open($fName, "a")
 	f.puts data
 	f.close
 end
 
 def readData
-	File.open($fName, "r") { |f|
-		s = f.read
-	}
-	
+	File.open($fName, "r") do |f|
+		s = "[ %s ]" % f.read().slice!(0..-3)
+	end
 end
